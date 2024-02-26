69ecf534a877569b59e2100156f34440687280a4
96rachidier <isaiaser1996@gmail.com>
2024-02-26 20:08:45 -0300

    agregué instrucción print

--- PrimitivosFloat.java
+++ PrimitivosFloat.java
@@ -1,5 +1,6 @@
 public class PrimitivosFloat { //sistema de punto flotante
     public static void main(String[] args) {
         float realFloat = 1;
+        System.out.println("realFloat= " + realFloat);
     }
 }
