1b902f265a2bded2b57d981c4abd5dc326306aed
96rachidier <isaiaser1996@gmail.com>
2024-02-26 20:34:15 -0300

    Cambié el valor de la variable float

--- PrimitivosFloat.java
+++ PrimitivosFloat.java
@@ -1,6 +1,6 @@
 public class PrimitivosFloat { //sistema de punto flotante
     public static void main(String[] args) {
-        float realFloat = 1;
+        float realFloat = 10000.25f; //si no agrego la letra f lo toma como double
         System.out.println("realFloat= " + realFloat);
     }
 }
