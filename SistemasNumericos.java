7ce25d50673061df09e601bba024a84c14c8266e
96rachidier <isaiaser1996@gmail.com>
2024-02-27 16:49:51 -0300

    agregué método para convertir enteros a binarios

--- SistemasNumericos.java
+++ SistemasNumericos.java
@@ -1,2 +1,9 @@
 public class SistemasNumericos {
+    public static void main(String[] args) {
+
+        int numeroDecimal = -100;
+        System.out.println("numeroDecimal = " + numeroDecimal);
+
+        System.out.println("El numero binario de " + numeroDecimal + " es " + Integer.toBinaryString(numeroDecimal)); //método para convertir números a binarios
+    }
 }
