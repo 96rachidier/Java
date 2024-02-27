3ac408e0d72590d5211da2cd372c012fa8bced4f
96rachidier <isaiaser1996@gmail.com>
2024-02-27 17:13:18 -0300

    agregué sistema hexadecimal

--- SistemasNumericos.java
+++ SistemasNumericos.java
@@ -18,7 +18,11 @@ public class SistemasNumericos {
         int numeroOctal = 0764; //le agrego un 0 para que java lo tome como un octal
         System.out.println("numeroOctal = " + numeroOctal);
 
+        /* numeros decimales a hexadecimal*/
+        System.out.println("numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
         
-
+        /*numero hexadecimal a entero*/
+        int numeroHex = 0x1f4; //le agrego 0x para indicarle que es un hexadecimal
+        System.out.println("numeroHex = " + numeroHex);
     }
 }
