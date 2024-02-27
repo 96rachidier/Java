88fb0e4afd9362ac86a929795c5cb3de95160e47
96rachidier <isaiaser1996@gmail.com>
2024-02-27 16:57:25 -0300

    agregué ejemplo para mostrar binarios como enteros

--- SistemasNumericos.java
+++ SistemasNumericos.java
@@ -1,9 +1,16 @@
 public class SistemasNumericos {
     public static void main(String[] args) {
 
-        int numeroDecimal = -100;
+        int numeroDecimal = 500;
         System.out.println("numeroDecimal = " + numeroDecimal);
 
+        /* conversor de números enteros a binarios*/
         System.out.println("El numero binario de " + numeroDecimal + " es " + Integer.toBinaryString(numeroDecimal)); //método para convertir números a binarios
+
+        /* números binarios a enteros*/
+        int numeroBinario = 0b111110100; //le agrego '0b' al número para que java lo tome como binario
+        System.out.println("numeroBinario = " + numeroBinario);
+
+        
     }
 }
