6669ed2637df0112ae67571d7fe7dc7ae373482e
96rachidier <isaiaser1996@gmail.com>
2024-02-27 16:03:47 -0300

    operación con un comparador entre un float y un double

--- Booleans.java
+++ Booleans.java
@@ -3,7 +3,13 @@ public class Booleans {
         boolean datoLogico = true; //true o false
         boolean datoLogico2 = Boolean.TRUE; //declaro una constante
         final boolean datoLogico3 = false; //declaro una constante de otra forma
-                System.out.println("datoLogico2 = " + datoLogico2);
-                System.out.println("datoLogico3 = " + datoLogico3);
+        System.out.println("datoLogico2 = " + datoLogico2);
+        System.out.println("datoLogico3 = " + datoLogico3);
+        /* ejemplo con comparador*/
+        double d = 2d;
+        float f = 10000f;
+
+        datoLogico = (d < f);
+        System.out.println("datoLogico = " + datoLogico);
     }
 }
