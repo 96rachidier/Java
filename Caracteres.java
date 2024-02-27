0babda5af70bfe5a516c951f03d65d5c5885e5bb
96rachidier <isaiaser1996@gmail.com>
2024-02-27 14:21:37 -0300

    Agregué clase caracteres

---
+++ Caracteres.java
@@ -0,0 +1,6 @@
+public class Caracteres {
+    public static void main(String[] args) {
+        char caracter = 'a'; //el char se declara con commilas simples y solo puede contener un valor
+        System.out.println("El caracter es: " + caracter);
+    }
+}
