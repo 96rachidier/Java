4584a32464b3cbe4ea34c4a7d0d5a4051b1a6c9f
96rachidier <isaiaser1996@gmail.com>
2024-02-27 15:56:12 -0300

    cree la clase y le asigné variables

---
+++ Booleans.java
@@ -0,0 +1,9 @@
+public class Booleans {
+    public static void main(String[] args) {
+        boolean datoLogico = true; //true o false
+        boolean datoLogico2 = Boolean.TRUE; //declaro una constante
+        final boolean datoLogico3 = false; //declaro una constante de otra forma
+                System.out.println("datoLogico2 = " + datoLogico2);
+                System.out.println("datoLogico3 = " + datoLogico3);
+    }
+}
