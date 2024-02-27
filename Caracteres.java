0bbce5942fa9125ebb834980b2d8248d37d716ba
96rachidier <isaiaser1996@gmail.com>
2024-02-27 14:48:50 -0300

    agregué ejemplos de char como modificador de cadenas

--- Caracteres.java
+++ Caracteres.java
@@ -1,8 +1,14 @@
 public class Caracteres {
     public static void main(String[] args) {
         char caracter = '$'; //el char se declara con comillas simples y solo puede contener un valor
-        char caracter2 = '\u0040'; //en characters maps de wiwndos puedo buscar la representación y debo ponerle un "\" + (representación)
+        char caracter2 = '\u0040'; //en characters maps de wiwndos puedo buscar la representación unicode y debo ponerle un "\" + (unicode)
         System.out.println("El caracter 1 es: " + caracter);
         System.out.println("El caracter 2 es: " + caracter2);
+        /*también puedo usar los char para editar cadenas*/
+        char tabulador = '\t';
+        char nuevaLinea = '\n';
+        System.out.println("El caracter 1 es: " + tabulador + caracter);
+        System.out.println("El caracter 2 es " + nuevaLinea + caracter2);
+
     }
 }
