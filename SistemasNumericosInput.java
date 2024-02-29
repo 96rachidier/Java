9cad8ab7261af12f15eb220f37c0ae1906fc8c75
96rachidier <isaiaser1996@gmail.com>
2024-02-29 17:51:55 -0300

    Agregué métodos de conversión de enteros

--- SistemasNumericosInput.java
+++ SistemasNumericosInput.java
@@ -3,11 +3,12 @@ import javax.swing.*;
 public class SistemasNumericosInput {
     public static void main(String[] args) {
 
-        String numeroStr = JOptionPane.showInputDialog(null,"Ingrese un numero entero"); //JOptionPane me proporciona métodos para mostrar ventanas
+        String numeroStr = JOptionPane.showInputDialog(null, "Ingrese un numero entero"); //JOptionPane me proporciona métodos para mostrar ventanas
         int numeroDecimal = Integer.parseInt(numeroStr); //el método parseInt convierte el input de la variable numeroStr en un Int
-        System.out.println("Numero entero = " + numeroDecimal);
-        System.out.println("El numero binario de " + numeroDecimal + " es " + Integer.toBinaryString(numeroDecimal)); //método para convertir números a binarios
-        System.out.println("numero Octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal));
-        System.out.println("numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
+        
+        String mensaje = ("Numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+        mensaje += "\nNumero octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal); //concatenación dinámica
+        mensaje += "\nNumero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal);
+        JOptionPane.showMessageDialog(null, mensaje);
     }
 }
