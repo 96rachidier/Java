5dc0b26ec8f5c0d7fdb6fd8d781c4240dca01a5c
96rachidier <isaiaser1996@gmail.com>
2024-03-05 12:16:31 -0300

    agregué strings

--- SistemasNumericosInput.java
+++ SistemasNumericosInput.java
@@ -14,10 +14,11 @@ public class SistemasNumericosInput {
             main(args);
             System.exit(0);
         }
-        System.out.println("numeroDecimal = " numeroDecimal);
+        System.out.println("numeroDecimal = " + numeroDecimal);
 
         String resultadoBinario = "numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal);
         String resultadoOctal = "numero octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal);
+        String resultadoHex = "numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal);
 
         String mensaje = ("Numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
         mensaje += "\nNumero octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal); //concatenación dinámica
